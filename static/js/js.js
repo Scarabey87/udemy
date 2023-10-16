94b33eccf40a867c5174190fc53fa2b1cd7bb5b5
jbw87 <caravagio2323@gmail.com>
2023-10-17 01:04:07 +0300

    asd

--- static/js/js.js
+++ static/js/js.js
@@ -1,47 +1,67 @@
-function User(uName, uAge, uBalance, uSkills) {
-    var _name = uName
-    this.age = uAge
-    this.balance = uBalance
-    this.skills = uSkills
-    this.getInfo = function() {
-        // console.log('Name: ' + this.name + ' Age: ' + this.age + ' Balance: ' + this)
-        document.getElementById('root').innerHTML = this.name + ' ' + this.age + ' ' + this.balance
-    }
-    if (this.age < 18) {
-        console.log('Attention')
-    }
-    this.getName = function() {
-        return _name
-    }
-    this.setName = function(uName) {
-        if (typeof uName === 'string' && uName.length > 2) {
-            _name = uName;
-        } else {
-            console.log('error')
-        }
-    }
-}
-// User.prototype.check = function() {
+// function User(uName, uAge, uBalance, uSkills) {
+//     var _name = uName
+//     this.age = uAge
+//     this.balance = uBalance
+//     this.skills = uSkills
+//     this.getInfo = function() {
+//         console.log('Name: ' + _name + ' Age: ' + this.age + ' Balance: ' + this.balance)
+//             // document.getElementById('root').innerHTML = this.name + ' ' + this.age + ' ' + this.balance
+//     }
 //     if (this.age < 18) {
-//         console.log('Too young')
-//     } else {
-//         console.log('Normal young')
+//         console.log('Attention')
+//     }
+//     this.getName = function() {
+//         return _name
+//     }
+//     this.setName = function(uName) {
+//         if (typeof uName === 'string' && uName.length > 2) {
+//             _name = uName;
+//         } else {
+//             console.log('error')
+//         }
 //     }
 // }
+// // User.prototype.check = function() {
+// //     if (this.age < 18) {
+// //         console.log('Too young')
+// //     } else {
+// //         console.log('Normal young')
+// //     }
+// // }
 
-function corp_user(uName, uAge, uBalance, uID) {
-    User.call(this, uName, uAge, uBalance)
-    this.id = uID
-    this.getInfo = function() {
-        console.log('Name: ' + this._name + ' Age: ' + this.age + ' Balance: ' + this.balance + ' ID: ' + this.id)
+// function corp_user(uName, uAge, uBalance, uID) {
+//     User.call(this, uName, uAge, uBalance)
+//     var _name = uName
+//     this.id = uID
+//     this.getInfo = function() {
+//         console.log('Name: ' + _name + ' Age: ' + this.age + ' Balance: ' + this.balance + ' ID: ' + this.id)
 
+//     }
+//     this.getName = function() {
+//         return _name
+//     }
+//     this.setName = function(uName) {
+//         if (typeof uName === 'string' && uName.length > 2) {
+//             _name = uName;
+//         } else {
+//             console.log('error')
+//         }
+//     }
+// }
+// corp_user.prototype = Object.create(User.prototype)
+// const u1 = new User('John', 35, 26750, ['Soft', 'Hard'])
+// const u2 = new User('S', 31, 17600)
+
+// const a3 = new corp_user('EEE', 15, 80000, 1)
+// a3.setName('Mary')
+// console.log(a3.getName())
+// a3.getInfo()
+
+class robots {
+    id
+    name
+    isReproduce
+    action(act) {
+        console.log(this.name + act)
     }
 }
-corp_user.prototype = Object.create(User.prototype)
-const u1 = new User('John', 35, 26750, ['Soft', 'Hard'])
-const u2 = new User('S', 31, 17600)
-
-const a3 = new corp_user('', 15, 80000, 1)
-a3.setName('Mary')
-console.log(a3.getName())
-a3.getInfo()
